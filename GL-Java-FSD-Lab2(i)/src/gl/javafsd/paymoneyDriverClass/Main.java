5b65b425b3804639f45acd0061e526d0fe997b4e
shiny <shinysujeetha217@gmail.com>
2022-04-11 23:43:55 +0530

    Adding files

---
+++ GL-Java-FSD-Lab2(i)/src/gl/javafsd/paymoneyDriverClass/Main.java
@@ -0,0 +1,12 @@
+package gl.javafsd.paymoneyDriverClass;
+
+import gl.javafsd.paymoneyTransactionUI.PayMoneyTransactionUI;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		PayMoneyTransactionUI ui=new PayMoneyTransactionUI();
+		ui.init();
+		
+	}
+}
---
+++ GL-Java-FSD-Lab2(ii)/src/gl/javafsd/travellersDriverClass/Main.java
@@ -0,0 +1,10 @@
+package gl.javafsd.travellersDriverClass;
+import gl.javafsd.travellersUI.*;
+
+public class Main {
+	public static void main(String[] args) {
+		TravellersUI ui =new TravellersUI();
+		ui.init();		
+	}
+
+}
